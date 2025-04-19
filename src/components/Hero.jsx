d3d54bb3bdb72e92956e8c39702411ea3a6ec359
Shubham Shinde <123376721+shubham-shinde-442@users.noreply.github.com>
2025-04-19 11:18:16 +0530

    Update Hero.jsx 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <section className='relative bg-green-600 text-white mt-12 md:mt-15'>
             <div className='max-w-6xl mx-auto py-16 flex flex-col md:flex-row items-center px-6 md:px-0 gap-7'>
                 <div className='md:w-1/2 mb-8 md:mb-0'>
-                    <h1 className='text-4xl md:text-5xl font-bold mb-4'>Fresh mart <span className='text-orange-400'>Groceries</span> Delivered to Your Door</h1>
+                    <h1 className='text-4xl md:text-5xl font-bold mb-4'>Fresh <span className='text-orange-400'>Groceries</span> Delivered to Your Door</h1>
                     <p className='text-lg mb-6'>Shop from our wide selection of fresh fruits, vegetables, dairy, and more. Get same-day delivery!</p>
                     <div className='flex flex-wrap gap-4 items-center'>
                         <Link to={'/shop'}><button className='cursor-pointer bg-white text-green-600 hover:bg-gray-100 rounded-full px-3 py-2'>Shop Now</button></Link>
